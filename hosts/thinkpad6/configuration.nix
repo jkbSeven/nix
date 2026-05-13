27ef1a1d8be0acd595d2a513e2f4b5d06e13cc51
jkbSeven <Jacob202@protonmail.com>
2026-05-13 14:53:28 +0200

    added gnome keyring service to thinkpad6 configuration

--- hosts/thinkpad6/configuration.nix
+++ hosts/thinkpad6/configuration.nix
@@ -110,6 +110,9 @@
     polkitPolicyOwners = [ "jkb" ];
   };
 
+  # keychain so that 1password and other apps can store credentials
+  services.gnome.gnome-keyring.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
